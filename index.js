ab4c805e106f4c507fd20a332d818164934d5e86
shilpasj <41962992+shilpasj@users.noreply.github.com>
2022-12-07 20:19:48 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+//console.log(`Hello Node.js v${process.versions.node}!`);
 function printNumbers() {
   var N = 100;
   for (var i = 0; i < N; i++) {
     if (i % 15 == 0) {
-      console.log('amazon');
+      document.write('amazon');
     } else if (i % 5 == 0) {
-      console.log('google');
+      document.write('google');
     } else if (i % 3 == 0) {
-      console.log('facebook');
+      document.write('facebook');
     } else {
-      console.log(i);
+      document.write(i);
     }
+    document.write('<br>');
   }
 }
 printNumbers();
